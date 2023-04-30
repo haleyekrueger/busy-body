8e9c2f7d54560a5c5301db739fb786fe3a11f6ab
mnell3 <nell.melissa3@gmail.com>
2023-04-29 22:07:49 -0700

    Pushing correct files

---
+++ app/home/stats.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const stats = () => {
+    return(
+        <View>
+            <Text>statistics</Text>
+        </View>
+    )
+}
+
+export default stats
