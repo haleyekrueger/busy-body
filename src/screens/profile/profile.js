fb565c7285eba0556650be4fe19ae7cf9347b19c
Haley Krueger <67514633+haleyekrueger@users.noreply.github.com>
2023-05-11 10:38:36 -0700

    Connecting profile.js to Users collection

--- src/screens/profile/profile.js
+++ src/screens/profile/profile.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import {useNavigation} from '@react-navigation/core';
+import axios from 'axios';
 
 import CustomInput from '../../components/CustomInput';
 import CustomButton from '../../components/CustomButton';
@@ -8,10 +9,45 @@ import CustomButton from '../../components/CustomButton';
 
 const Profile = () => {
     const navigation=useNavigation();
+    const [userData, setUserData] = useState(null);
 
     const onLogoutPressed = () => {
         navigation.navigate('Login')
       };
+    
+    useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/user'); # change url to host (probably appspot)
+        setUserData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+    
+  if (!userData) {
+    return <Text>Hang tight...</Text>;
+  }
+
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.root}>
+        <Text style={styles.title}>Profile</Text>
+        <Text style={styles.text}>Username: {userData.username}</Text>
+        <Text style={styles.text}>Age: {userData.age}</Text>
+        <Text style={styles.text}>Body Type: {userData.body_type}</Text>
+        <CustomButton
+          text="Log Out"
+          onPress={onLogoutPressed}
+          type="SECONDARY"
+        />
+      </View>
+    </ScrollView>
+  );
+};
 
     return(
     <ScrollView showsVerticalScrollIndicator={false}>
