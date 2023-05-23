ee85ddf5053b0415cd0d9798cf1befd584e8621c
Daniel <72196528+sadokist@users.noreply.github.com>
2023-05-22 19:29:16 -0700

    Update signUp.js

--- src/screens/signUp/signUp.js
+++ src/screens/signUp/signUp.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, TextInput, StyleSheet, ScrollView } from 'react-native';
 import { useNavigation} from '@react-navigation/core'
 
 import CustomInput from '../../components/CustomInput';
@@ -8,35 +8,7 @@ import SocialSignInButton from '../../components/SocialSignInButtons';
 
 
 //resource: https://www.youtube.com/watch?v=ALnJLbjI7EY
-const config = {
-  backendUrl: 'https://busy-body-386417.wn.r.appspot.com',
-}
-const body_type = "mesomorph";
-const age = 26; 
-const registerUser = async (username, password, age, body_type) => {
-  try {
-    const response = await fetch(`${config.backendUrl}/users`, {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify({
-        username,
-        password,
-        age,
-        body_type
-      }),
-    });
-    if (response.ok) {
-      return true;
-    } else {
-      const errorData = await response.json()
-      throw new Error(errorData.message)
-    }
-  } catch (error) {
-    throw new Error ('Registration failed.')
-  }
-}
+
 const SignUp = () => {
 
   const navigation = useNavigation();
@@ -45,15 +17,72 @@ const SignUp = () => {
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
   const [repeatPassword, setRepeatPassword] = useState('');
+  const [seePassword, setSeePassword] = useState(true);
+ 
 
+  const [errors, setErrors] = React.useState({});
+  const [loading, setLoading] = React.useState(false);
 
-  const onRegisterPressed = async() => {
-    try{
-      await registerUser(username, password, age, body_type);
-      navigation.navigate('ConfirmEmail')
-    } catch (error) {
-      console.error(error)
+  const validate = () => {
+
+    handleError(null, 'email');
+    handleError(null, 'password');
+    handleError(null, 'repeatPassword');
+    //Keyboard.dismiss();
+    let isValid = true;
+    let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+
+    if (!email) {
+      handleError('Please input an email', 'email');
+      isValid = false;
+    } else if (reg.test(email) === false) {
+
+      handleError('Please input a valid email', 'email');
+      isValid = false;
     }
+
+    if (!password) {
+      handleError('Please input password', 'password');
+      isValid = false;
+    } else if (password.length < 5) {
+      handleError('Password must be greater than 5 characters', 'password');
+      isValid = false;
+    }
+
+    const isNonWhiteSpace = /^\S*$/;
+    const isContainsUppercase = /^(?=.*[A-Z]).*$/;
+    const isContainsNumber = /^(?=.*[0-9]).*$/;
+
+    if (!isNonWhiteSpace.test(password)) {
+      handleError('Password must not contain whitespace', 'password');
+      isValid = false;
+    }
+
+    if (!isContainsUppercase.test(password)) {
+      handleError('Password must have at least 1 uppercase letter', 'password');
+      isValid = false;
+    }
+
+    if (!isContainsNumber.test(password)) {
+      handleError('Password must contain at least 1 number', 'password');
+      isValid = false;
+    }
+
+    if (password != repeatPassword) {
+      console.log("not the same passwords")
+      handleError('Passwords do not match', 'repeatPassword');
+      isValid = false;      
+    }
+
+    if (isValid) {
+      //register();
+      console.log("valid");
+    }
+  };
+
+
+  const onRegisterPressed = () => {
+    navigation.navigate('ConfirmEmail')
   };
 
   const onTermsOfUsePressed = () => {
@@ -67,8 +96,51 @@ const SignUp = () => {
   const onSignInPress = () => {
     navigation.navigate('Login')
   };
+  
+/*
+  const checkPasswordValidity = value => {
+    const isNonWhiteSpace = /^\S*$/;
+    if (!isNonWhiteSpace.test(value)) {
+      return 'Password must not contain Whitespaces.';
+    }
 
+    const isContainsUppercase = /^(?=.*[A-Z]).*$/;
+    if (!isContainsUppercase.test(value)) {
+      return 'Password must have at least one Uppercase Character.';
+    }
 
+    const isContainsLowercase = /^(?=.*[a-z]).*$/;
+    if (!isContainsLowercase.test(value)) {
+      return 'Password must have at least one Lowercase Character.';
+    }
+
+    const isContainsNumber = /^(?=.*[0-9]).*$/;
+    if (!isContainsNumber.test(value)) {
+      return 'Password must contain at least one Digit.';
+    }
+
+    const isValidLength = /^.{8,16}$/;
+    if (!isValidLength.test(value)) {
+      return 'Password must be 8-16 Characters Long.';
+    }
+
+    // const isContainsSymbol =
+    //   /^(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).*$/;
+    // if (!isContainsSymbol.test(value)) {
+    //   return 'Password must contain at least one Special Symbol.';
+    // }
+
+    return null;
+  };*/
+
+  const handleOnchange = (text, input) => {
+    setInputs(prevState => ({...prevState, [input]: text}));
+  };
+  const handleError = (error, input) => {
+    setErrors(prevState => ({...prevState, [input]: error}));
+  };
+
+  
     return (
       <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.root}>
@@ -77,30 +149,45 @@ const SignUp = () => {
             <CustomInput 
             placeholder="Username"
             value={username}
+            onChangeText={setUsername}
             setValue={setUsername}
+            secureTextEntry={false}
             />
 
             <CustomInput
             placeholder="Email"
             value={email}
+            onChangeText={text => handleOnchange(text, 'email')}
+            onFocus={() => handleError(null, 'email')}
             setValue={setEmail}
+            secureTextEntry={false}
+            error={errors.email}
             />
 
             <CustomInput
             placeholder="Password"
             value={password}
+            onChangeText={text => handleOnchange(text, 'password')}
+            onFocus={() => handleError(null, 'password')}
             setValue={setPassword}
+            secureTextEntry={true}
+            error={errors.password}
             />
 
             <CustomInput
             placeholder="Re-enter Password"
             value={repeatPassword}
+            onChangeText={text => handleOnchange(text, 'repeatPassword')}
+            onFocus={() => handleError(null, 'repeatPassword')}
             setValue={setRepeatPassword}
+            secureTextEntry={true}
+            error={errors.repeatPassword}
             />      
 
             <CustomButton 
             text="Register" 
-            onPress={onRegisterPressed}
+            //onPress={onRegisterPressed}
+            onPress={validate}
             />
 
             <Text style={styles.text}>
@@ -112,7 +199,7 @@ const SignUp = () => {
             <SocialSignInButton/>
 
             <CustomButton
-            text="Already have an account? Sign intest"
+            text="Already have an account? Sign in"
             onPress={onSignInPress}
             type="TERTIARY"
             />
